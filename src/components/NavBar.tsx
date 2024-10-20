9dcb7bfa9476e2eddf3865763f60310d7e29d0bc
Mohamed-Mos-aad <ma3268787@gmail.com>
2024-10-20 13:35:32 +0300

    mobile burger menu finished

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import style from '../style/components/navbar.module.css'
 
 
@@ -5,16 +6,44 @@ import style from '../style/components/navbar.module.css'
 
 
 export default function NavBar() {
+    // ** States
+    const [mobileMenuOpen,setMobileMenuOpen] = useState<boolean>(false);
+    // ** Handlers
+    const mobileMenuToggleHandler = ()=>{
+        setMobileMenuOpen(!mobileMenuOpen);
+    } 
+
+
+
+
+
+
+
     return (
         <>
-            <nav className={style.navBar_container}>
-                <div className={style.logo}>
-                    <h2>LinkGo</h2>
+            <nav className={style.navBar}>
+                <div className={style.navBar_container}>
+                    <div className={style.logo}>
+                        <h2>LinkGo</h2>
+                    </div>
+                    <div className={style.auth_btns}>
+                        <button>Login</button>
+                        <button>Sign Up</button>
+                    </div>
+                    <div className={`${style.mob_menu_btn} ${mobileMenuOpen&& style.menu_open}`} onClick={mobileMenuToggleHandler}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
-                <div className={style.auth_btns}>
-                    <button>Login</button>
-                    <button>Sign Up</button>
-                </div>
+                {mobileMenuOpen &&
+                    <div className={style.mob_menu_content}>
+                        <div className={style.mob_auth_btns}>
+                            <button>Login</button>
+                            <button>Sign Up</button>
+                        </div>
+                    </div>
+                }
             </nav>
         </>
     )
